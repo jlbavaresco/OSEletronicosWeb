04959d76de79e6fd46f6a372a7d856b0b02aca7b
Jorge <Jorge@jorge-inspirion>
2018-09-26 18:08:13 -0300

    Commit inicial do projeto

---
+++ src/java/br/edu/ifsul/dao/EstadoDAO.java
@@ -0,0 +1,20 @@
+package br.edu.ifsul.dao;
+
+import br.edu.ifsul.modelo.Estado;
+import java.io.Serializable;
+import javax.ejb.Stateful;
+
+/**
+ * @author Prof. Me. Jorge Luis Boeira Bavaresco
+ * @email jorge.bavaresco@passofundo.ifsul.edu.br
+ * @organization IFSUL - Campus Passo Fundo
+ */
+@Stateful
+public class EstadoDAO<TIPO> extends DAOGenerico<Estado> implements Serializable {
+
+    public EstadoDAO(){
+        super();
+        classePersistente = Estado.class;
+        ordem = "nome";
+    }
+}
