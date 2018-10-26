d0e31b80e655774f2d811a2978feae9f90c23580
Jorge <Jorge@jorge-inspirion>
2018-10-25 21:28:11 -0300

    Ajustes no cadastro de usuário para verificar o o nome já existe ao passar pelo campo.

--- src/java/br/edu/ifsul/controle/ControleUsuario.java
+++ src/java/br/edu/ifsul/controle/ControleUsuario.java
@@ -8,6 +8,11 @@ import br.edu.ifsul.modelo.Usuario;
 import br.edu.ifsul.util.Util;
 import java.io.Serializable;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -20,7 +25,7 @@ import javax.inject.Named;
 @Named(value = "controleUsuario")
 @ViewScoped
 public class ControleUsuario implements Serializable {
-    
+
     @EJB
     private UsuarioDAO<Usuario> dao;
     private Usuario objeto;
@@ -28,31 +33,31 @@ public class ControleUsuario implements Serializable {
     @EJB
     private PermissaoDAO<Permissao> daoPermissao;
     private Permissao permissao;
-    
+
     public ControleUsuario() {
-        
+
     }
-    
+
     public String listar() {
         novoObjeto = false;
         return "/privado/usuario/listar?faces-redirect=true";
     }
-    
+
     public void novo() {
         novoObjeto = true;
-        objeto = new Usuario();        
+        objeto = new Usuario();
     }
-    
+
     public void alterar(Object id) {
         try {
             novoObjeto = false;
-            objeto = dao.getObjectById(id);            
+            objeto = dao.getObjectById(id);
         } catch (Exception e) {
             Util.mensagemErro("Erro ao recuperar objeto: "
                     + Util.getMensagemErro(e));
-        }        
+        }
     }
-    
+
     public void excluir(Object id) {
         try {
             objeto = dao.getObjectById(id);
@@ -63,75 +68,95 @@ public class ControleUsuario implements Serializable {
                     + Util.getMensagemErro(e));
         }
     }
-    
+
     public void salvar() {
         try {
             if (novoObjeto) {
-                dao.persist(objeto);                
+                dao.persist(objeto);
             } else {
-                dao.merge(objeto);                
+                dao.merge(objeto);
             }
-            
-            Util.mensagemInformacao("Objeto persistido com sucesso!");            
+
+            Util.mensagemInformacao("Objeto persistido com sucesso!");
         } catch (Exception e) {
             Util.mensagemErro("Erro ao persistir objeto: "
                     + Util.getMensagemErro(e));
         }
     }
-    
+
     public void salvarPermissao() {
         if (!objeto.getPermissoes().contains(permissao)) {
-            objeto.getPermissoes().add(permissao);            
-            Util.mensagemInformacao("Permissão adicionada com sucesso!");            
+            objeto.getPermissoes().add(permissao);
+            Util.mensagemInformacao("Permissão adicionada com sucesso!");
         } else {
             Util.mensagemErro("Usuário já possui esta permissão!");
         }
-        
+
     }
-    
+
     public void excluirPermissao(Permissao obj) {
-        objeto.getPermissoes().remove(obj);        
-        Util.mensagemInformacao("Permissão removida com sucesso!");        
+        objeto.getPermissoes().remove(obj);
+        Util.mensagemInformacao("Permissão removida com sucesso!");
     }
-    
+
+    public void verificarUnicidadeNomeUsuario() {
+        if (novoObjeto) {
+            try {
+                if (!dao.verificaUnicidadeNomeUsuario(objeto.getNomeUsuario())) {
+                    Util.mensagemErro("Nome de usuário '" + objeto.getNomeUsuario() + "' já existe no banco de dados");
+                    // Capturando o componente que chamou o método
+                    UIComponent comp = UIComponent.getCurrentComponent(FacesContext.getCurrentInstance());
+                    if (comp != null) {
+                        // se não for nulo faço um cast no componente para input 
+                        // e coloco o setValid(false) para o campo ficar em vermelho após o update
+                        UIInput input = (UIInput) comp;
+                        input.setValid(false);                        
+                    }
+                }
+            } catch (Exception e) {
+                Util.mensagemErro("Erro do sistema: " + Util.getMensagemErro(e));
+            }
+        }
+    }
+
     public UsuarioDAO getDao() {
         return dao;
     }
-    
+
     public void setDao(UsuarioDAO dao) {
         this.dao = dao;
     }
-    
+
     public Usuario getObjeto() {
         return objeto;
     }
-    
+
     public void setObjeto(Usuario objeto) {
         this.objeto = objeto;
     }
-    
+
     public Boolean getNovoObjeto() {
         return novoObjeto;
     }
-    
+
     public void setNovoObjeto(Boolean novoObjeto) {
         this.novoObjeto = novoObjeto;
     }
-    
+
     public PermissaoDAO<Permissao> getDaoPermissao() {
         return daoPermissao;
     }
-    
+
     public void setDaoPermissao(PermissaoDAO<Permissao> daoPermissao) {
         this.daoPermissao = daoPermissao;
     }
-    
+
     public Permissao getPermissao() {
         return permissao;
     }
-    
+
     public void setPermissao(Permissao permissao) {
         this.permissao = permissao;
     }
-    
+
 }
--- src/java/br/edu/ifsul/dao/UsuarioDAO.java
+++ src/java/br/edu/ifsul/dao/UsuarioDAO.java
@@ -4,6 +4,7 @@ import br.edu.ifsul.converters.ConverterOrdem;
 import br.edu.ifsul.modelo.Usuario;
 import java.io.Serializable;
 import javax.ejb.Stateful;
+import javax.persistence.Query;
 
 /**
  * @author Prof. Me. Jorge Luis Boeira Bavaresco
@@ -31,5 +32,22 @@ public class UsuarioDAO<TIPO> extends DAOGenerico<Usuario> implements Serializab
         // Deve-se inicializar as coleções para não gerar erro de LazyInicializationException
         obj.getPermissoes().size();
         return obj;
-    }    
+    }   
+    
+    /**
+     * Método que verifica se o nome de usuário já existe
+     * @param nomeUsuario nome do usuário a ser verificado
+     * @return false se já existe e true se não existe usuário com o nome informado
+     * @throws Exception 
+     */
+    public boolean verificaUnicidadeNomeUsuario(String nomeUsuario) throws Exception {       
+            String jpql = "from Usuario where nomeUsuario = :pNomeUsuario";
+            Query query = em.createQuery(jpql);
+            query.setParameter("pNomeUsuario", nomeUsuario);
+            if (query.getResultList().size() > 0){
+                return false;
+            } else {
+                return true;
+            }       
+    }
 }
