bddb8b84da25a15a2e951992d376779d6be01da3
Jorge <Jorge@jorge-inspirion>
2019-02-06 14:50:32 -0200

    Listagem e geração de parcelas no CRUD de OS, e envio de fotos.

--- src/java/br/edu/ifsul/controle/ControleOrdemServico.java
+++ src/java/br/edu/ifsul/controle/ControleOrdemServico.java
@@ -1,16 +1,16 @@
 package br.edu.ifsul.controle;
 
-
 import br.edu.ifsul.dao.EquipamentoDAO;
 import br.edu.ifsul.dao.OrdemServicoDAO;
 import br.edu.ifsul.dao.PessoaFisicaDAO;
 import br.edu.ifsul.dao.ProdutoDAO;
 import br.edu.ifsul.dao.ServicoDAO;
 import br.edu.ifsul.dao.UsuarioDAO;
+import br.edu.ifsul.modelo.Arquivo;
 import br.edu.ifsul.modelo.Equipamento;
 import br.edu.ifsul.modelo.FormaPagamento;
+import br.edu.ifsul.modelo.Foto;
 import br.edu.ifsul.modelo.ItemServico;
-
 
 import br.edu.ifsul.modelo.OrdemServico;
 import br.edu.ifsul.modelo.PessoaFisica;
@@ -19,12 +19,21 @@ import br.edu.ifsul.modelo.Servico;
 import br.edu.ifsul.modelo.Status;
 import br.edu.ifsul.modelo.Usuario;
 import br.edu.ifsul.util.Util;
+import java.io.ByteArrayInputStream;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 /**
  *
@@ -33,9 +42,9 @@ import javax.inject.Named;
  * @organization IFSUL - Campus Passo Fundo
  */
 @Named(value = "controleOrdemServico")
-@ViewScoped
+@SessionScoped
 public class ControleOrdemServico implements Serializable {
-    
+
     @EJB
     private OrdemServicoDAO<OrdemServico> dao;
     private OrdemServico objeto;
@@ -51,100 +60,164 @@ public class ControleOrdemServico implements Serializable {
     private ProdutoDAO<Produto> daoProduto;
     private ItemServico itemServico;
     private Boolean novoItemServico;
-    
-    public ControleOrdemServico(){
-        
+    private Foto foto;
+
+    public ControleOrdemServico() {
+
     }
-       
-    public String listar(){
+
+    public String listar() {
         return "/privado/ordemservico/listar?faces-redirect=true";
     }
-    
-    public void novo(){
-        objeto = new OrdemServico();        
+
+    public void novo() {
+        objeto = new OrdemServico();
     }
-        
-    public void alterar(Object id){
+
+    public void alterar(Object id) {
         try {
-            objeto = dao.getObjectById(id);            
-        } catch (Exception e){
-            Util.mensagemErro("Erro ao recuperar objeto: " + 
-                    Util.getMensagemErro(e));
-        } 
+            objeto = dao.getObjectById(id);
+        } catch (Exception e) {
+            Util.mensagemErro("Erro ao recuperar objeto: "
+                    + Util.getMensagemErro(e));
+        }
     }
-    
-    public void excluir(Object id){
+
+    public void excluir(Object id) {
         try {
             objeto = dao.getObjectById(id);
             dao.remover(objeto);
             Util.mensagemInformacao("Objeto removido com sucesso!");
-        } catch (Exception e){
-            Util.mensagemErro("Erro ao remover objeto: " + 
-                    Util.getMensagemErro(e));
+        } catch (Exception e) {
+            Util.mensagemErro("Erro ao remover objeto: "
+                    + Util.getMensagemErro(e));
         }
     }
-    
-    public void salvar(){
+
+    public void salvar() {
         try {
-            if (objeto.getId() == null){
+            if (objeto.getId() == null) {
                 dao.persist(objeto);
             } else {
                 dao.merge(objeto);
             }
-            Util.mensagemInformacao("Objeto persistido com sucesso!");            
-        } catch(Exception e){            
-            Util.mensagemErro("Erro ao persistir objeto: " + 
-                    Util.getMensagemErro(e));
+            Util.mensagemInformacao("Objeto persistido com sucesso!");
+        } catch (Exception e) {
+            Util.mensagemErro("Erro ao persistir objeto: "
+                    + Util.getMensagemErro(e));
         }
     }
-    
-    public void novoItemServico(){
+
+    public void novoItemServico() {
         itemServico = new ItemServico();
         novoItemServico = true;
-    }    
-    
-    public void alterarItemServico(int index){
+    }
+
+    public void alterarItemServico(int index) {
         itemServico = objeto.getListaServicos().get(index);
         novoItemServico = false;
     }
-    
-    public void salvarItemServico(){
-        if (novoItemServico){
+
+    public void salvarItemServico() {
+        if (novoItemServico) {
             objeto.adicionarServico(itemServico);
         } else {
             atualizarValorTotal();
         }
         Util.mensagemInformacao("Serviço adicionado com sucesso");
-    }  
-    
-    public void removerItemServico(int index){
+    }
+
+    public void removerItemServico(int index) {
         objeto.removerServico(index);
         Util.mensagemInformacao("Serviço removido com sucesso");
-    }     
-  
-    public void atualizaValorUnitarioItemServico(){
-        if (itemServico != null){
-            if (itemServico.getServico()!= null){
+    }
+
+    public void atualizaValorUnitarioItemServico() {
+        if (itemServico != null) {
+            if (itemServico.getServico() != null) {
                 itemServico.setValorUnitario(itemServico.getServico().getValor());
             }
         }
-    }   
-    
-    public void calculaValorTotalItemServico(){
+    }
+
+    public void calculaValorTotalItemServico() {
         if (itemServico.getValorUnitario() != null
-                && itemServico.getQuantidade() != null){
+                && itemServico.getQuantidade() != null) {
             itemServico.setValorTotal(itemServico.getValorUnitario() * itemServico.getQuantidade());
         }
-    }  
-    
+    }
+
     private void atualizarValorTotal() {
         objeto.setValorTotal(0.0);
         Double total = 0.0;
-        for (ItemServico is : objeto.getListaServicos()){
+        for (ItemServico is : objeto.getListaServicos()) {
             total += is.getValorTotal();
         }
         objeto.setValorTotal(total);
-    }        
+    }
+
+    public void gerarParcelas() {
+        objeto.getContasReceber().clear();
+        objeto.gerarContasReceber();
+    }
+
+    public void novaFoto() {
+        foto = new Foto();
+    }
+
+    public void salvarFoto() {
+        objeto.adicionarFoto(foto);
+        Util.mensagemInformacao("Foto adicionada com sucesso!");
+    }
+
+    public void removerFoto(int index) {
+        objeto.removerFoto(index);
+        Util.mensagemInformacao("Foto removida com sucesso!");
+    }
+
+    public void enviarFoto(FileUploadEvent event) {
+        try {
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+            FacesContext aFacesContext = FacesContext.getCurrentInstance();
+            ServletContext context = (ServletContext) aFacesContext.getExternalContext().getContext();
+            foto.setArquivo(event.getFile().getContents());
+            String nomeFoto = event.getFile().getFileName();
+            nomeFoto = nomeFoto.replaceAll("[ ]", "_");
+            foto.setNomeFoto(nomeFoto);
+            Util.mensagemInformacao("Foto enviada com sucesso!");
+        } catch (Exception e) {
+            Util.mensagemErro("Erro ao enviar foto: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public void downloadFoto(int index) {
+        foto = objeto.getFotos().get(index);
+        byte[] download = (byte[]) foto.getArquivo();
+        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        response.addHeader("Content-Disposition", "attachment; filename=" + foto.getNomeFoto());
+        response.setContentLength(download.length);
+        try {
+            response.setContentType("application/octet-stream");
+            response.getOutputStream().write(download);
+            response.getOutputStream().flush();
+            FacesContext.getCurrentInstance().responseComplete();
+        } catch (Exception e) {
+            Util.mensagemErro("Erro no download da foto: " + Util.getMensagemErro(e));
+        }
+    }
+
+    public void visualizarFoto(int index) {
+        foto = objeto.getFotos().get(index);
+    }
+
+    public StreamedContent getImagemDinamica() {
+        if (foto != null) {
+            return new DefaultStreamedContent(new ByteArrayInputStream(foto.getArquivo()), "");
+        } else {
+            return new DefaultStreamedContent();
+        }
+    }
 
     public OrdemServicoDAO getDao() {
         return dao;
@@ -218,7 +291,12 @@ public class ControleOrdemServico implements Serializable {
         this.novoItemServico = novoItemServico;
     }
 
+    public Foto getFoto() {
+        return foto;
+    }
 
-
+    public void setFoto(Foto foto) {
+        this.foto = foto;
+    }
 
 }
